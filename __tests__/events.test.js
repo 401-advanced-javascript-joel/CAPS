a5c4f40d3acb0aa7bdeee10957aa0c02a870aee4
Joel Watson <Joel.watson92@gmail.com>
2020-05-09 11:54:19 -0700

    Updated tests to match new tests. also fixed last test (was false positive)

--- __tests__/events.test.js
+++ __tests__/events.test.js
@@ -4,6 +4,11 @@ const events = require('../lib/events');
 const Vendor = require('../lib/vendor');
 const Driver = require('../lib/driver');
 
+const driver = new Driver(events);
+const vendor = new Vendor(events);
+driver.listen();
+vendor.listen();
+
 let consoleSpy = jest.spyOn(console, 'log');
 
 const order = {
@@ -14,64 +19,59 @@ const order = {
   address: `12404 SE 217th CT, Kent, WA`,
 };
 
-describe('Testing the pickup event handling', () => {
-  test('Should log that order was picked up from driver', () => {
-    const driver = new Driver(events);
-    driver.listen();
-    events.emit('pickup', order);
+beforeEach(() => {
+  consoleSpy.mockClear();
+});
 
+describe('Testing the pickup event handling', () => {
+  test('Should log that order was picked up from driver', async () => {
+    events.emit('pickup', order);
     expect(consoleSpy).toHaveBeenCalledWith(
-      'Driver: Picking up order ad68c0f2-2735-41ef-a80f-0faa8263b29a.',
+      'DRIVER: Picked up order ad68c0f2-2735-41ef-a80f-0faa8263b29a.',
     );
-    consoleSpy.mockClear();
   });
 });
 
 describe('Testing the in-transit event handling', () => {
   test('Should log that order is in transit', () => {
-    const driver = new Driver(events);
-    driver.listen();
     events.emit('in-transit', order);
     expect(consoleSpy).toHaveBeenCalledWith(
-      'Event: in-transit - ad68c0f2-2735-41ef-a80f-0faa8263b29a',
+      'EVENT: in-transit order ad68c0f2-2735-41ef-a80f-0faa8263b29a',
     );
-    consoleSpy.mockClear();
   });
 });
 
 describe('Testing the delivered event handling', () => {
   test('should log thank you from vendor', () => {
-    const vendor = new Vendor(events);
-    vendor.listen();
     events.emit('delivered', order);
     expect(consoleSpy).toHaveBeenCalledWith(
       'VENDOR: Thank you for delivering order ad68c0f2-2735-41ef-a80f-0faa8263b29a',
     );
-    consoleSpy.mockClear();
   });
 });
 
 describe('testing vendor module', () => {
   test('should generate a new order', () => {
-    const vendor = new Vendor(events);
     vendor.generateOrder();
     expect(consoleSpy).toHaveBeenCalled();
-    consoleSpy.mockClear();
   });
 });
 
-describe('testing driver module', () => {
-  test('should take 3 seconds to handle deliver', async () => {
-    const driver = new Driver(events);
+describe('testing Driver module', () => {
+  test('should take 3 seconds to handle deliver', (done) => {
+    jest.useFakeTimers();
     driver.inTransit(order);
-    expect(consoleSpy).toHaveBeenCalledWith(
-      'Driver: Driving order ad68c0f2-2735-41ef-a80f-0faa8263b29a to customer.',
-    );
-    await setTimeout(() => {
-      expect(consoleSpy).toHaveBeenCalledWith(
-        'Driver: Driving order ad68c0f2-2735-41ef-a80f-0faa8263b29a to customer.',
-        'Event: Delivered - ad68c0f2-2735-41ef-a80f-0faa8263b29a',
-      );
+    expect(consoleSpy).not.toHaveBeenCalled();
+    setTimeout(() => {
+      try {
+        expect(consoleSpy).toHaveBeenCalledWith(
+          'DRIVER: Delivered order ad68c0f2-2735-41ef-a80f-0faa8263b29a',
+        );
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
     }, 3000);
+    jest.runTimersToTime(3000);
   });
 });
